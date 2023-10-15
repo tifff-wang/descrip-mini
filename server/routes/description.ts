83f82192a157fa9a8e29950cc737e55e7a490928
Tiffany Wang <thiffanyjun@hotmail.com>
2023-10-16 11:23:41 +1300

    add instructions to connect and disconnet to api.

--- server/routes/description.ts
+++ server/routes/description.ts
@@ -1,22 +1,22 @@
 import express from 'express'
 import { Configuration, OpenAIApi } from 'openai'
-import request from 'superagent'
 import 'dotenv/config'
 import { Keywords } from '../../models/formKeywords'
 
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-  // const keywords = req.body
-  // console.log(keywords)
-  // const answer = await getAnswer(keywords)
-  // res.json(answer.data.choices[0].message)
-  const response = {
-    role: 'assistant',
-    content: 'Hello! How can I assist you today?',
-  }
+  //uncomment line 10-13 and comment 15-19 to call the OpenAI api.
+  const keywords = req.body
+  console.log(keywords)
+  const answer = await getAnswer(keywords)
+  res.json(answer.data.choices[0].message)
 
-  res.json(response)
+  // const response = {
+  //   role: 'assistant',
+  //   content: 'Hello World, this paragraph is for testing purpose, comment line 10-13 and uncomment 15-19 to fake the api process.',
+  // }
+  // res.json(response)
 })
 
 async function getAnswer(form: Keywords) {
@@ -30,7 +30,7 @@ async function getAnswer(form: Keywords) {
     messages: [
       {
         role: 'user',
-        // content: `Generate a 2 sentence description for my second-hand selling based on the keywords of ${form.itemName}, ${form.category}, ${form.condition}, ${form.brand},and ${form.location}`,
+        content: `I want to sell my ${form.itemName} on Facebook marketplace, can you generate a 3 sentence description for my second-hand selling listing with keywords: item name: ${form.itemName} from category: ${form.category}, condition: ${form.condition}, brand: ${form.brand},and pick up from: ${form.location}`,
         // content: 'hello world',
       },
     ],
